079824763a9f26a1fa6bc5a6850ce86459fd6519
anuamreddy47486@gmail.com <anumareddy47486@gmail.com>
2023-11-07 16:21:01 -0600

    Added Configuration. java and LimitController.java files

--- src/main/java/com/rap/Microservices/LimitService/controller/LimitController.java
+++ src/main/java/com/rap/Microservices/LimitService/controller/LimitController.java
@@ -1,14 +1,19 @@
 package com.rap.Microservices.LimitService.controller;
 
 import com.rap.Microservices.LimitService.bean.Limits;
+import com.rap.Microservices.LimitService.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitController {
+    @Autowired
+    private Configuration configuration;
     @GetMapping(path="/limits")
     public Limits retriveLimits(){
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 
 
